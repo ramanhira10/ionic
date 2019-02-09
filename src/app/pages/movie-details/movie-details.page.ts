138cd79fb74763af69366d78f35b3b3bd8f3d6bc
raman <ramanhira@gmail.com>
2019-02-09 12:19:49 +0530

    created iconic-movie-app with imdb's api

---
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+import { MovieService } from '../../services/movie.service';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.page.html',
+  styleUrls: ['./movie-details.page.scss'],
+})
+export class MovieDetailsPage implements OnInit {
+
+  information = null;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private movieService: MovieService
+  ) { }
+
+  ngOnInit() {
+
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.movieService.getDetails(id).subscribe(result => this.information = result);
+  }
+
+  openWebsite () {
+    window.open(this.information.Website, '_blank');
+  }
+
+
+}
---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export enum SearchType {
+  all = '',
+  movie = 'movie',
+  series = 'series',
+  episode = 'episode'
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  
+  url = 'http://www.omdbapi.com/';
+  apikey = 'd3c1c9a0';
+
+  constructor(private http: HttpClient) { }
+
+  searchData (title: string, type: SearchType): Observable<any> {
+
+    return this.http
+      .get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apikey}`)
+      .pipe(map(results => results['Search']));
+  }
+
+  getDetails (id) {
+    return this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apikey}`);
+  }
+
+}
